a9334849cbf192edcd288314ff253d624c5ace2a
nateraw <nxr9266@g.rit.edu>
2022-11-02 16:46:32 -0700

    :construction: wip

--- prepare_comment.py
+++ prepare_comment.py
@@ -1,6 +1,4 @@
 import argparse
-import os
-import subprocess
 from pathlib import Path
 
 from tabulate import tabulate
@@ -10,36 +8,23 @@ def parse_args(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("--root_dir", type=str, required=True)
     parser.add_argument("--repo_id", type=str, required=True)
+    parser.add_argument("--commit_sha", type=str, required=True)
     return parser.parse_args(args)
 
 
-def get_branch(root):
-    with open(os.devnull, "wb") as null_stream:
-        result = subprocess.check_output(
-            f"cd {root} && git rev-parse --abbrev-ref HEAD",
-            shell=True,
-            stdin=null_stream,
-            stderr=null_stream,
-        )
-    result = result.decode('utf-8')
-    result = result.strip()
-    return result
-
-def main(root_dir: str, repo_id: str):
+def main(root_dir: str, repo_id: str, commit_sha: str):
     root_dir = Path(root_dir)
     if not root_dir.exists():
         raise ValueError(f"root_dir {root_dir} does not exist")
-
-    branch = get_branch(root_dir)
 
     notebook_paths = sorted(root_dir.glob("**/*.ipynb"))
 
     table_data = []
     for notebook_path in notebook_paths:
         relpath = notebook_path.relative_to(root_dir)
-        gh_link = f"https://github.com/{repo_id}/blob/{branch}/{relpath}"
+        gh_link = f"https://github.com/{repo_id}/blob/{commit_sha}/{relpath}"
         gh_link_md = f"[{relpath}]({gh_link})"
-        colab_link = f"https://colab.research.google.com/github/{repo_id}/blob/{branch}/{relpath}"
+        colab_link = f"https://colab.research.google.com/github/{repo_id}/blob/{commit_sha}/{relpath}"
         colab_badge_md = f"[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)]({colab_link})"
         row = [gh_link_md, colab_badge_md]
         table_data.append(row)
